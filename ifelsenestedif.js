e84fa81fd600194b551b2e87a25d2a00cd8bab94
Fadhil899 <blackfadhil34@gmail.com>
2023-10-08 20:58:10 +0700

    first commit

---
+++ ifelsenestedif.js
@@ -0,0 +1,21 @@
+let num = 20;
+
+if (num > 5) {
+  console.log("20");
+} else if (num < 5) {
+  console.log("-7");
+} else {
+  console.log("0");
+}
+
+if (num > 8) {
+  if (num % 2 === 6) {
+    console.log("5");
+  } else {
+    console.log("12");
+  }
+} else if (num < 4) {
+  console.log("-6");
+} else {
+  console.log("0");
+}
